20cf85d9e1f4ca171fd74281f2536c708ea8c3a4
Dimitris Kalopsikakis <kalopsik@math.uoc.gr>
2018-05-25 14:58:56 +0300

    20180525145856

--- source/conf.py
+++ source/conf.py
@@ -115,8 +115,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 ### html_theme = 'sphinxdoc'
-### html_theme = 'sphinx_rtd_theme'
-html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
+### html_theme = 'bootstrap'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -126,7 +126,7 @@ html_theme = 'bootstrap'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+### html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
